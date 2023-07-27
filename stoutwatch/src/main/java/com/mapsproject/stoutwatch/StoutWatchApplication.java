0db3aa3a6e896b8f6dcfde8bfe097e2388db7dc6
Callan John Maguire <callanjohn.maguire@version1.com>
2023-07-27 12:08:08 +0100

    Add Spring Boot Start Files
    - Start files relevant to the creation of this spring boot project
      added.

---
+++ stoutwatch/src/main/java/com/mapsproject/stoutwatch/StoutWatchApplication.java
@@ -0,0 +1,13 @@
+package com.mapsproject.stoutwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoutWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoutWatchApplication.class, args);
+	}
+
+}
---
+++ stoutwatch/src/test/java/com/mapsproject/stoutwatch/StoutWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapsproject.stoutwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoutWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
